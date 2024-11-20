105936cbc1b291df4b07157e594d4295cee4bdda
Nillton Pimenta <njqp@icomp.ufam.edu.br>
2024-11-20 18:20:19 -0400

    Connecting to the database Atlas MongoDB

--- server.js
+++ server.js
@@ -1,42 +1,68 @@
 import express from "express";
 
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto teste",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 2,
-        descricao: "Gato fazendo yoga",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-    {
-        id: 3,
-        descricao: "Gato fazendo panqueca",
-        imagem: "https://placecats.com/millie/300/150",
-    },
-];
+import conectarAoBanco from "./src/config/dbconfig.js";
+// Importa a função para conectar ao banco de dados, definida no arquivo dbconfig.js.
 
-const app = express(); // Usar o express
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+// Conecta ao banco de dados usando a string de conexão obtida da variável de ambiente.
+
+// const posts = [
+//     {
+//         id: 1,
+//         descricao: "Uma foto teste",
+//         imagem: "https://placecats.com/millie/300/150",
+//     },
+//     {
+//         id: 2,
+//         descricao: "Gato fazendo yoga",
+//         imagem: "https://placecats.com/millie/300/150",
+//     },
+//     {
+//         id: 3,
+//         descricao: "Gato fazendo panqueca",
+//         imagem: "https://placecats.com/millie/300/150",
+//     },
+// ];
+
+const app = express();
+//Cria uma instância do aplicativo Express.
+
 app.use(express.json());
+// Habilita o parsing de corpos de requisição no formato JSON.
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-}); // Escutar o servidor na porta 3000
-
-app.get("/posts", (requisicao, resposta) => {
-    resposta.status(200).json(posts);
-    // resposta.status(200).send("Boas vindas à imersão!");
 });
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console.
 
-function buscaPostPorID(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
+async function getTodosPosts() {
+    const db = conexao.db("imersao-instabytes");
+    // Seleciona o banco de dados "imersao-instabytes".
+
+    const colecao = db.collection("posts");
+    // Seleciona a coleção "posts" dentro do banco de dados.
+
+    return colecao.find().toArray();
+    // Retorna todos os documentos da coleção como um array.
 }
 
-app.get("/posts/:id", (requisicao, resposta) => {
-    const index = buscaPostPorID(requisicao.params.id);
-    resposta.status(200).json(posts[index]);
+app.get("/posts", async (requisicao, resposta) => {
+    const posts = await getTodosPosts();
+    // Obtém todos os posts chamando a função getTodosPosts.
+    resposta.status(200).json(posts);
+    // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON.
+
+    // resposta.status(200).send("Boas vindas à imersão!");
+    // Linha editada
 });
+
+// function buscaPostPorID(id) {
+//     return posts.findIndex((post) => {
+//         return post.id === Number(id);
+//     });
+// }
+
+// app.get("/posts/:id", (requisicao, resposta) => {
+//     const index = buscaPostPorID(requisicao.params.id);
+//     resposta.status(200).json(posts[index]);
+// });
